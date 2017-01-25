9911fcd328468c9e7bc7db832300f20a777ccc5c
kimbuca <kimbuca.rola@gmail.com>
2017-01-25 13:30:02 -0600

    added api delete

--- app/router/forms/controller.js
+++ app/router/forms/controller.js
@@ -6,43 +6,51 @@ var Record			= require('./model/record');
 
 exports.newEntry = function(req, res){
 
-		//console.log(req.body);
+	var user = new Record();
 
-		var user = new Record();
+	user.name 			= req.body.name;
+	user.lname 			= req.body.lname;
+	user.nationality 	= req.body.nation;
+	user.email			= req.body.email;	
+	user.locale 		= req.body.locale;
+	user.comment		= req.body.comments;
+	user.speciality 	= req.body.speciality;
+	user.status			= req.body.status;
 
-		user.name 			= req.body.name;
-		user.lname 			= req.body.lname;
-		user.nationality 	= req.body.nation;
-		user.email			= req.body.email;	
-		user.locale 		= req.body.locale;
-		user.comment		= req.body.comments;
-		user.speciality 	= req.body.speciality;
-		user.status			= req.body.status;
+	user.save(function(err){
 
-		user.save(function(err){
+		if(err){
+			res.send(err);
+		}
 
-			if(err){
-				res.send(err);
-			}
-
-			res.json({
-				message: "something was created, dont know what yet"
-			});
-
-
-		})
-		
+		res.json({
+			message: "something was created, dont know what yet"
+		});
+	})
 };
 
 
 exports.fetch = function(req, res){
 
-		Record.find(function(err, data){
+	Record.find(function(err, data){
 
-			if(err){
-				res.send(err);
-							
-			}
-			res.send(data);
-		});
+		if(err){
+			res.send(err);
+						
+		}
+		res.send(data);
+	});
+};
+
+
+exports.delete = function(req, res){
+
+	Record.remove({'_id': req.params.rec_id}, function(err, rec){
+		if(err){
+			res.send(err);
+		}
+
+		res.json({message: "Record Deleted !"});
+	});
+
 };
